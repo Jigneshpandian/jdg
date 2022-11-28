4d3938c3ea8eadd8cd13acbaa3ede477cd48f267
Jigneshpandian <jigneshpandian007@gmail.com>
2022-11-28 14:19:08 +0530

    Files added

---
+++ html.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input");
+const body = document.querySelector("body");
+
+
+const toggleThemeMode = () => {
+    body.classList.toggle("dark");
+};
+
+input.onchange = toggleThemeMode;
